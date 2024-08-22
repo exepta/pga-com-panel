4faea66aad3f9896eab0cec8ea9b889a52df0c63
exepta <genshin.exepta@outlook.com>
2024-08-22 19:32:25 +0200

    feat: create new container for same TextFields name (RegTextField.tsx)

---
+++ src/pages/authentication-page/models/RegTextField.tsx
@@ -0,0 +1,49 @@
+import {Box, TextField, useTheme} from "@mui/material";
+
+type Props = {
+    id: string,
+    name: string,
+    type?: string,
+}
+
+const RegTextField = ({id, name, type}: Props) => {
+    const theme = useTheme();
+    return (
+        <Box sx={{
+            width: '100%',
+            height: '80px',
+            display: 'flex',
+            alignItems: 'center',
+            boxSizing: 'border-box',
+            borderRadius: '8px'
+        }}>
+            <TextField id={id} label={name} fullWidth variant="outlined" type={type}
+                       sx={{
+                           '& .MuiOutlinedInput-root': {
+                               '& fieldset': {
+                                   borderColor: theme.colors.placeholder,
+                                   borderWidth: '2px',
+                               },
+                               '&:hover fieldset': {
+                                   borderColor: theme.colors.accent,
+                               },
+                               '&.Mui-focused fieldset': {
+                                   borderColor: theme.colors.accent,
+                               },
+                               '& input': {
+                                   color: theme.colors.primary,
+                               },
+                           },
+
+                           '& .MuiInputLabel-root': {
+                               color: theme.colors.placeholder,
+                           },
+                           '& .MuiInputLabel-root.Mui-focused': {
+                               color: theme.colors.accent,
+                           },
+                       }} />
+        </Box>
+    );
+}
+
+export default RegTextField;
